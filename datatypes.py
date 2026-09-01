503768d94a9eb8403253046b529939aec7770558
sabribechillin <hharper865@boyslatin.net>
2026-09-01 15:06:59 +0000

    datatype notes

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+#string- characters that are wrapped within qoutes.
+#characters can be letters. numbers, symbols
+#you can use both single and double quotation marks for strings
+#marks for strings 
+"hyaire harper"
+"this cost $100
+#the hashtag is a marker that makes anything written behind it invisable to the compute
+integers- datatypes that are just a whole numbers, or numbers with decimal point.
+10
+20
+20000000
+floats- is just a decimal number.
+boolean-a datatype that can be either true or false.
+
+
