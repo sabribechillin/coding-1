3c9fae7a9a53fcb18530af2d84667f3e926b32e6
sabribechillin <hharper865@boyslatin.net>
2026-09-22 15:08:35 +0000

    class notes

--- operatorsActivity.py
+++ operatorsActivity.py
@@ -7,3 +7,38 @@ print("coding1" )#true
 
 
 print("0"!=0)  #true
+
+
+
+print(cart>amountogetdiscount)
+
+
+    book=10.99
+    tablet=399.999
+) 
+
+print(200.00 x 0.07 )#false
+
+
+print(
+    #A college app program that checks if a# gpa is above 85 and 
+    print(85>gpa  and )
+print(GPA > 85 and recommendation=true)
+
+
+GPA= 86
+
+
+
+
+
+parent contact=false
+parent contact=true
+print(parentcontact== or parent contact2== true)
+
+
+
+
+
+
+
---
+++ unit2/builtinFuctions.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+#function- simply put; a code block of 
+#instuctions for computers to follow.
+
+#that was already written for us. 
+
+
+#data casting functions
+#these are built in (pre-written instructions)
+#that change datac types from one from into another
+
+#str()- this datacasting functions allows you to to change 
+#any data type into a string
+
+
+
+
+year=1906
+print("this event took place in+str(year)")
+
+
+#string concatenation - combing string data types
+#with one another. we use the plus sign+ operator 
+# to combine strings togetheer
+
+print(3+3)# adding 2 integers
+print("7+4")#combining 2 strings
+print("good"+ "bye"
+
+
+
+
+
+      #int()- any data type passed into the brackets will
+      ## to be converted into a integer(whole number)
+      #always returns a string
+      num1= input(type in a number;'')
+      print(4+num1)
+
